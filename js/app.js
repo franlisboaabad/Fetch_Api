318d4ad5dc40e01c8790a943f8c3f5f5ff92adef
franklisboaabad <franco_lisboa@hotmail.com>
2020-04-15 18:37:39 -0500

    Aplicando footer al landing page

--- js/app.js
+++ js/app.js
@@ -1,20 +1,11 @@
-const button1 = document.getElementById('button_1');
-const button2 = document.getElementById('button_2');
-const button3 = document.getElementById('button_3');
-
 
 $('#modal_1').on('shown.bs.modal', function () {
     $('#button_1').trigger('focus')
 })
 
-
-
-
 $('#modal_2').on('shown.bs.modal', function () {
     $('#button_2').trigger('focus')
 })
-
-
 
 $('#modal_3').on('shown.bs.modal', function () {
     $('#button_3').trigger('focus')
